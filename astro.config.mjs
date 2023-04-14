8327b2c496d46713faa3d0633fa459a15f05932b
Albert Utomo <me@albertutomo.com>
2023-04-14 23:52:10 +0800

    feat: installed @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
